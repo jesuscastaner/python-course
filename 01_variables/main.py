dc3cffb2951c93a4685feef2551bd8dfda2372c1
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-10-08 15:11:36 +0200

    feat: add 01_variables

---
+++ 01_variables/main.py
@@ -0,0 +1,30 @@
+# variable = a container for a value (string, integer, float, boolean)
+#            a variable behaves as if it was the value it contains
+
+# strings
+name = "Jesus"
+food = "pizza"
+
+print(f"Hello, {name}!")
+print(f"You like {food}")
+
+# integers
+age = 29
+num_students = 30
+
+print(f"You are {age} years old")
+print(f"Your class has {num_students} students")
+
+# floats
+height = 1.71
+distance = 5.5
+
+print(f"Your height is {height}m")
+print(f"You ran {distance}km")
+
+# boolean
+is_student = True
+is_online = False
+
+print(f"Are you a student?: {is_student}")
+print(f"Are you a online?: {is_online}")
